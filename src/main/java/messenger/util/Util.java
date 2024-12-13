52f09f9f6c3603db1e0a78223932e62c9d95f604
Сан Саныч <118749282+Salopogibshim@users.noreply.github.com>
2024-12-14 02:09:42 +0300

    Create Util.java

---
+++ src/main/java/messenger/util/Util.java
@@ -0,0 +1,19 @@
+package notificator.util;
+
+public class Util {
+    public static final String SUCCESS = "Success";
+
+    public static void throwException(String notificator) {
+        try {
+            throw new Exception(notificator);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void checkAuthorizedAccess(Boolean checkNotUser, Boolean checkMatchChat) {
+        if (checkNotUser && !checkMatchChat) {
+            throwException("Unauthorized access attempts");
+        }
+    }
+}
