4a1b672557e61e004ced08f5ad12309095ea9e56
Сан Саныч <118749282+Salopogibshim@users.noreply.github.com>
2024-12-14 02:07:07 +0300

    Create MessengerApplication.java

---
+++ src/main/java/messenger/MessengerApplication.java
@@ -0,0 +1,11 @@
+package notificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessengerApplication.class, args);
+    }
+}
