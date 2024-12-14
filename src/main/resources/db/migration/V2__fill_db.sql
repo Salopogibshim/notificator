3bb53e06f219930e939828943d3121c7545c7b64
Сан Саныч <118749282+Salopogibshim@users.noreply.github.com>
2024-12-14 11:45:16 +0300

    Create V2__fill_db.sql

---
+++ src/main/resources/db/migration/V2__fill_db.sql
@@ -0,0 +1,15 @@
+insert into "user" (id, first_name, last_name, sur_name, status)
+values ('1','Иванов','Иван','Иванович','USER'),
+       ('2','Петров','Петр','Петрович','SUPPORT'),
+       ('3','Сидоров','Сидор','Сидорович','USER'),
+       ('4','Тестов','Тест','Тестович','SUPPORT');
+
+insert into filter (id, user_id, types_ids)
+values ('1','2','{1, 3, 5}'),
+       ('2','3','{}');
+
+insert into notificator (id, filter_id, author_id, title, message, created_at, status)
+values ('1','1','1','Мало средств на карте','Казна пустеет пополните ее',now(),'UNREAD'),
+       ('2','1','2','Задолжность не погашена','Вам начислят завтра пени',2,now(),'UNREAD'),
+       ('3','2','3','Новое предложение','МММ снова в моде',3,now(),'UNREAD'),
+       ('5','2','4','Супер выгодный микрозайм','Возьмешь ты, а погасят твои дети',5,now(),'UNREAD');
